16f7b7677086e574e234b3597c89c48dfd54bdd3
Mottaaaa <pedro10mota@msn.com>
2018-11-27 02:33:11 +0000

    database migration

---
+++ ProjetoESW/ProjetoESW/Data/Migrations/20181127023150_ajustes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoESW.Data.Migrations
+{
+    public partial class ajustes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "Nome");
+        }
+    }
+}
