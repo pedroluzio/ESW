76448372c0786eeb32f662f5d25b658d64af89c6
Mottaaaa <pedro10mota@msn.com>
2018-11-27 02:08:46 +0000

    Ajuste Users

--- ProjetoESW/ProjetoESW/Models/User.cs
+++ ProjetoESW/ProjetoESW/Models/User.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProjetoESW.Areas.Identity.Data
+namespace ProjetoESW.Models
 {
     public class User : IdentityUser
     {
