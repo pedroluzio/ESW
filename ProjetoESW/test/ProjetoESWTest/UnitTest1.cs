710ec832a67d23caebe386fb8b3091cc4031f1ec
Rick2323 <Ricardo.C.Professional@hotmail.com>
2019-01-21 19:00:22 +0000

    bosta

--- ProjetoESW/test/ProjetoESWTest/UnitTest1.cs
+++ ProjetoESW/test/ProjetoESWTest/UnitTest1.cs
@@ -5,10 +5,22 @@ namespace ProjetoESWTest
 {
     public class UnitTest1
     {
+
         [Fact]
         public void Test1()
         {
 
+            //preciso de um ApplicationDbContext!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111
+
+            var mockContext = new Mock<ApplicationDbContext>();
+            //var controller = new MembrosController(new ClubeContext(new DbContextOptions<ClubeContext>()));
+
+            //var controller = new MembrosController(mockContext.Object);
+
+
+            //var result = controller.AteMaioridade(idade);
+
+           // Assert.Equal(expected, result);
         }
     }
 }
